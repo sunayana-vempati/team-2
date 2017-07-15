22ce97144b036a1432b026bf961e36b88a202212
lokeshlukalapu <lukalapu.lokesh@gmail.com>
2017-07-16 04:08:11 +0530

    signup

--- signup.php
+++ signup.php
@@ -46,11 +46,11 @@ div {
 
 <h2 style="margin-left:250px;color:green;">Welcome</h2>
 
-<!--<div style="width:50%;margin-left:250px" >
+<div style="width:50%;margin-left:250px" >
     <form action="" method="post">
 	<label>Type:</label>
 		<span style="color:red">*</span>
-        <input type="radio" name="user" value="scribe"> scribe<br>
+        <input type="radio" name="user" value="scribe" checked> scribe<br>
         <input type="radio" name="user" value="student"> student<br>
       
         <label >Name:</label>
@@ -75,7 +75,7 @@ div {
 
         <input type="submit" value="Submit" name="Submit">
     </form>
-	</div>-->
+	</div>
 <?php
 
 
@@ -98,9 +98,9 @@ $nameErr = $emailErr = $genderErr = $websiteErr = "";
 $name1 = $password1=$email1 = $phno1=$gender1 =  "";
 
 
-if (isset($_POST["submit"])) {
+if (isset($_POST["Submit"])) {
 	
-if($_POST["user"]=="student")
+if($_POST["user"]=="scribe")
 	{
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
@@ -150,22 +150,80 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $gender1 = test_input($_POST["gender"]);
   }
   
-  echo $name1;
   
-$sql = "INSERT INTO scribe (name, email,password,gender,phoneno)
+  
+$sql = "INSERT INTO scribe(name, email,password,gender,phoneno)
 VALUES ('$name1', '$email1','$password1', '$gender1','$phno1')";
 if (mysqli_query($conn, $sql)) {
     echo "New record created successfully";
-	?><script>
-	window.location="stulogin.php";
-	</script><?php
+	//header('location:stulogin.php');
+	
 } else {
     echo "Error: " ;
 }
 }
 
 	}else{
-		
+		if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+  if (empty($_POST["name"])) {
+    $nameErr = "Name is required";
+  } else {
+    $name1 = test_input($_POST["uname"]);
+    // check if name only contains letters and whitespace
+    if (!preg_match("/^[a-zA-Z ]*$/",$name1)) {
+      $nameErr = "Only letters and white space allowed"; 
+    }
+  }
+  
+  if (empty($_POST["pwd"])) {
+    $nameErr = "Password is required";
+  } else {
+    $password1 = test_input($_POST["pwd"]);
+    // check if name only contains letters and whitespace
+    
+  }
+  
+  
+  if (empty($_POST["phno"])) {
+    $nameErr = "Name is required";
+  } else {
+    $phno1 = test_input($_POST["phno"]);
+    // check if name only contains letters and whitespace
+    
+  }
+  
+  if (empty($_POST["email"])) {
+    $emailErr = "Email is required";
+  } else 
+  {
+    $email1= test_input($_POST["email"]);
+    // check if e-mail address is well-formed
+    if (!filter_var($email1, FILTER_VALIDATE_EMAIL)) {
+      $emailErr = "Invalid email format"; 
+    }
+  }
+    
+  
+
+
+  if (empty($_POST["gender"])) {
+    $genderErr = "Gender is required";
+  } else {
+    $gender1 = test_input($_POST["gender"]);
+  }
+  
+  
+  
+$sql = "INSERT INTO stureg (name, email,password,gender,phoneno)
+VALUES ('$name1', '$email1','$password1', '$gender1','$phno1')";
+if (mysqli_query($conn, $sql)) {
+    echo "New record created successfully";
+	header('location:scribelogin.php');
+} else {
+    echo "Error: " ;
+}
+}
 	}
 }
 
