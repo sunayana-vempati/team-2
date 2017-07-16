29311eae6540448bb559c94cc9841c6e797e6007
lokeshlukalapu <lukalapu.lokesh@gmail.com>
2017-07-16 07:41:53 +0530

    lokesh

--- scribenew.php
+++ scribenew.php
@@ -91,7 +91,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 	$email=$_POST["emailid"];
 	$address= $_POST["address"];
 	
-    // check if name only contains letters and whitespace
+    // check if name only contains letters 
     
   }
 }
